ead96e876f00dcad7efde9e3c393a42b788307a0
Igor Polischuk <polischuk01@gmail.com>
2023-03-18 21:29:46 +0200

    implemented resize window method

--- src/ts/World/World.ts
+++ src/ts/World/World.ts
@@ -2,18 +2,29 @@ import { Camera } from "./Camera";
 import { Scene } from "./Scene";
 import { Renderer } from "./Renderer";
 
-export class World{
+export class World {
     private scene = new Scene().scene
     private camera = new Camera().camera
     private renderer = new Renderer().renderer
 
-    constructor(){
+    constructor() {
         this.renderer.setAnimationLoop(this.update.bind(this))
-        
+        this.changeWindowWhenResize()
     }
 
-    update(){
-        
+    update() {
+
         this.renderer.render(this.scene, this.camera)
+    }
+
+    private changeWindowWhenResize() {
+        window.addEventListener('resize', () => this.onWindowResize(), false);
+        this.onWindowResize();
+    }
+
+    private onWindowResize() {
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
     }
 }
