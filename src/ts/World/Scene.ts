229c48306f54129a2cf4aa32a22f67959c9bf10b
Igor Polischuk <polischuk01@gmail.com>
2023-03-18 21:06:26 +0200

    implemented scene class

--- src/ts/World/Scene.ts
+++ src/ts/World/Scene.ts
@@ -1,3 +1,8 @@
-export class Scene{
-    
+import * as THREE from 'three'
+
+export class Scene {
+    public scene = new THREE.Scene()
+    constructor() {
+        this.scene.background = new THREE.Color('skyblue');
+    }
 }
