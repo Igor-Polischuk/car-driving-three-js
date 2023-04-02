8eae4040112863ebd339ddc273c2a1d8c1b7804b
Igor Polischuk <polischuk01@gmail.com>
2023-04-02 23:07:39 +0300

    implemented gears

--- src/ts/components/Car/CarController.ts
+++ src/ts/components/Car/CarController.ts
@@ -21,6 +21,8 @@ export class CarController {
     private maxSpeed = 20
     private maxBackwardSpeed = -5
 
+    private gear = 4
+
     private isPlayingSound = false
 
     private FPS = 1
@@ -36,6 +38,7 @@ export class CarController {
 
     calculateCar(deltaTime: number) {
         this.FPS = 1 / deltaTime
+        this.changeGear()
         this.calculateVelocity()
         this.calculateDirection()
         this.soundsController()
@@ -47,7 +50,7 @@ export class CarController {
     private soundsController() {
         const engineSound = this.car.children[4] as THREE.Audio
         if (engineSound) {
-            engineSound.setVolume(this.velocity / this.maxSpeed * 4);
+            engineSound.setVolume(this.velocity / this.maxSpeed * this.gear);
             if (this.velocity !== 0 && !this.isPlayingSound) {
                 engineSound.play();
                 this.isPlayingSound = true
@@ -55,6 +58,23 @@ export class CarController {
                 engineSound.pause();
                 this.isPlayingSound = false
             }
+        }
+
+    }
+
+    private changeGear(){
+        if (this.pressedKeys.Digit1){
+            this.gear = 1
+            this.maxSpeed = 5
+        }else if(this.pressedKeys.Digit2){
+            this.gear = 2
+            this.maxSpeed = 10
+        }else if(this.pressedKeys.Digit3){
+            this.gear = 3
+            this.maxSpeed = 15
+        }else if(this.pressedKeys.Digit4){
+            this.gear = 3
+            this.maxSpeed = 20
         }
 
     }
@@ -101,7 +121,7 @@ export class CarController {
                 return
             }
             if (this.velocity >= maxVelocity) {
-                this.velocity = maxVelocity
+                this.velocity -= acceleration
                 return
             }
             this.velocity += acceleration
